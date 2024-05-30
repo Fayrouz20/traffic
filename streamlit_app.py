b2072bf2d70d2f95863b908c0092137342ec92e2
Fayrouz abdelaali <aitabdelaalifayrouz01@gmail.com>
2024-05-30 19:04:23 +0200

    add requerement file

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
+import os
+os.environ["PAFY_BACKEND"] = "internal"
+
 import streamlit as st
 import torch
 from train import Model  # Assurez-vous que le fichier train.py est dans le même répertoire
 import matplotlib.pyplot as plt
+from shapely.geometry import LineString, Point, Polygon
 
 # Fonction pour charger le modèle
 def load_model(model_path, input_dims, fc1_dims, fc2_dims, n_actions):
@@ -21,9 +25,13 @@ def main():
     fc2_dims = st.sidebar.number_input("Dimensions de la deuxième couche cachée", 256)
     n_actions = st.sidebar.number_input("Nombre d'actions", 4)
 
+    model = None
     if st.sidebar.button("Charger le modèle"):
-        model = load_model(model_path, input_dims, fc1_dims, fc2_dims, n_actions)
-        st.success("Modèle chargé avec succès")
+        try:
+            model = load_model(model_path, input_dims, fc1_dims, fc2_dims, n_actions)
+            st.success("Modèle chargé avec succès")
+        except Exception as e:
+            st.error(f"Erreur lors du chargement du modèle : {e}")
 
     st.header("Visualisation des résultats")
     if model:
@@ -31,6 +39,8 @@ def main():
         # Ici, vous pouvez ajouter du code pour visualiser les prédictions du modèle
         # Ex: ax.plot(data, predictions)
         st.pyplot(fig)
+    else:
+        st.warning("Veuillez charger un modèle pour afficher les résultats")
 
 if __name__ == "__main__":
     main()
